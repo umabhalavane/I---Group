24596235b97e48fc9a8770405e8baffed270ac79
Ankita Mozar <ankita.mozar@gmail.com>
2022-09-25 23:30:50 +0530

    fefe
    vsdv

---
+++ NewProject/src/stringex/Ex3.java
@@ -0,0 +1,30 @@
+package stringex;
+
+import javax.management.openmbean.OpenMBeanAttributeInfo;
+
+public class Ex3 {
+
+	
+	public static void main(String[] args) {
+		
+		String s = "java programs";
+		
+		System.out.println(s.toUpperCase());
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
